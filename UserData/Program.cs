9790ba3769fa319a82134e303248214ff76b4e77
Melissa Ots <meots@tktk.ee>
2021-09-14 00:48:49 +0300

    Control Flow

---
+++ UserData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust konsoolist;
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOFBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOFBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            //string interpolation
+
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+
+        }
+    }
+}
